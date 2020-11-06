604237a07bceeac8a4351ad5c49b3619b42cde90
avfallon <31700889+avfallon@users.noreply.github.com>
2020-11-06 14:45:45 -0500

    Laid out pet object

---
+++ app/src/main/java/com/example/petsitterapp/Pet.java
@@ -0,0 +1,48 @@
+public class Pet {
+		public int petID;
+
+		public Pet(String[] attributeList, String DBName) {
+				this.petID = addPet(attributeList);
+				//instantiate SQL database
+		}
+
+		/**
+		 * Purpose: add a new row to the Pet table with the input information
+		 * Input: a list of values for every field in the Pet table of the DB
+		 * Return: the integer pet ID, primary key for the Pet table
+		 */
+		public int addPet(String[] info) {
+
+		}
+
+		/**
+		 * Purpose: return the information of a given pet
+		 * Input: the primary key ID of the pet
+		 * Return: A list of the pet's information, or null if the pet cannot be found
+		 */
+		public String[] getPetInfo(int petID) {
+
+		}
+
+		/**
+		 * Purpose: edit the information of an existing pet in the DB
+		 * Input: a list of the new values to be put in the DB
+		 * Return: true if the pet row is successfully edited
+		 */
+		public boolean editPet(Stirng[] newInfo) {
+
+		}
+
+		/**
+		 * Purpose: remove an existing pet from a pet owner's account
+		 * Input: none
+		 * Return: true if the pet was successfully deleted, false if pet is not found
+		 */
+		public boolean deletePet() {
+
+		}
+
+
+
+
+}
