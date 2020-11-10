ddd645f2ac281375f9d2a391aa1106c06dc59bc5
damorales9 <damorales@loyola.edu>
2020-11-10 17:40:47 -0500

    php

--- app/src/main/php/query.php
+++ app/src/main/php/query.php
@@ -7,9 +7,26 @@
 
  $result = $mysqli->query("select * from Pets");
 
+ $array = [];
  while($row = $result->fetch_assoc())
  {
-   echo $row['Name'], "\n";
+   array_push($array, [
+     'ownerIDKey' => $row['OwnerIDKey'],
+     'petIDKey' => $row['PetIDKey'],
+     'name' => $row['Name'],
+     'species' => $row['Species'],
+     'size' => $row['Size'],
+     'temperament' => $row['Temperament'],
+     'breed' => $row['Breed'],
+     'age' => $row['Age'],
+     'diet' => $row['Diet'],
+     'healthIssues' => $row['HealthIssues'],
+     'extraInfo' => $row['ExtraInfo']
+   ]);
  }
 
-?>
+ $result = json_encode($array);
+
+ echo $result;
+
+ ?>
