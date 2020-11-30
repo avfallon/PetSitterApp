75eb335aebf3ec0222f7737df11fe95a36a00eec
damorales9 <damorales@loyola.edu>
2020-11-30 14:21:34 -0500

    query

--- app/src/main/php/query.php
+++ app/src/main/php/query.php
@@ -1,7 +1,6 @@
 <?php
  include( "constants.php");
  $username = Constants::USERNAME;
- echo "username is $username\n";
 
  $mysqli = new mysqli( Constants::HOST, $username, Constants::PASSWORD, Constants::DATABASE);
 
