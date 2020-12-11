7f2cc62253b1c82967d7d91d08d91363185be7f2
damorales9 <damorales@loyola.edu>
2020-12-11 02:00:00 -0500

    php

---
+++ app/src/main/php/deleteJob.php
@@ -0,0 +1,28 @@
+<?php
+  // Assign JSON encoded string to a PHP variable
+  include( "constants.php");
+  $username = Constants::USERNAME;
+
+  $mysqli = new mysqli( Constants::HOST, $username, Constants::PASSWORD, Constants::DATABASE);
+
+  $json = $_GET['json'];
+  //$json = '{"ownerIDKey":"4","petIDKey":"5","name":"Jeff","species":"Unknown","size":"Small","temperament":"Lazy","breed":"german shep","age":"21","diet":"Peanut Butter","healthIssues":"Needs Glasses","extraInfo":"Extras"}';
+  //$json = '{"ownerIDKey":"1","firstName":"Derek","lastName":"Morales","address":"123 New York Ave, New York, New York 01234","phoneNumber":"1233456789","email":"derek@derek.com","typeOfAccount":"1","password":"password"}';
+
+  // Decode JSON data to PHP object
+  $obj = json_decode($json);
+  // Access values from the returned object
+
+  $jobID = $obj->jobID;
+  $ownerIDKey = $obj->ownerIDKey;
+  $petIDKey = $obj->petIDKey;
+  $startDate = $obj->startDate;
+  $endDate = $obj->endDate;
+  $sleepover = $obj->sleepover;
+  $jobDetails = $obj->jobDetails;
+  $sitterIDKey = $obj->sitterIDKey;
+
+  $query = "DELETE from Jobs WHERE jobID = $jobID";
+  $result = $mysqli->query($query);
+  echo "Job Deleted Sucessfully";
+?>
