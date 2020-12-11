83e3bd593f0f676ce25404c611fb03bd472556ec
Jeffrey Umanzor <jjumanzor@loyola.edu>
2020-12-11 04:13:21 -0500

    Commit

--- app/src/main/java/com/example/petsitterapp/PetActivity.java
+++ app/src/main/java/com/example/petsitterapp/PetActivity.java
@@ -117,6 +117,16 @@ public class PetActivity extends AppCompatActivity {
         adapter_size.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
         spinner_size.setAdapter(adapter_size);
+
+        //SET PETSIZE SPINNER
+        Spinner spinner_temp = (Spinner) findViewById(R.id.PetForm_PetTemperamentSpinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter_temp = ArrayAdapter.createFromResource(this,
+                R.array.petSize_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter_temp.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner_temp.setAdapter(adapter_size);
     }
 
 
